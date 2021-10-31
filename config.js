a7d7d62cb302a397a026040243a0960cafcdf5ea
elementemerald <22267679+elementemerald@users.noreply.github.com>
2021-10-30 21:12:15 -0600

    Initial commit

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  "title": "EmeraldSys Status",
+  "name": "emeraldsys_status",
+  "description": "",
+  "defaultLocale": "en",
+  "locales": [
+    {
+      "code": "en",
+      "iso": "en-US",
+      "name": "English",
+      "file": "en.json"
+    }
+  ],
+  "content": {
+    "frontMatterFormat": "json"
+  }
+}
